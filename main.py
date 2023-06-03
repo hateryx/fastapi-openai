3d71d778916f25d959adbb589b4f57608fbc0300
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-06-03 22:41:46 +0800

    check

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, HTTPException
+import requests
+
+import os
+from dotenv import load_dotenv
+
+app = FastAPI()
+load_dotenv()
+
+OPEN_API_KEY = os.getenv('OPEN_API_KEY')
+
+
+@app.post("/api/process-prompt")
+async def process_prompt(prompt):
+
+    response = await requests.post(
+        "https://api.openai.com/v1/engines/davinci-codex/completions",
+        headers={
+            "Authorization": f"Bearer {OPEN_API_KEY}",
+            "Content-Type": "application/json",
+        },
+        json={
+            "prompt": prompt,
+            "max_tokens": 1000,
+            "model": "gpt-3.5-turbo"
+        }
+    )
+
+    # Check if the request to the OpenAI API was successful
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail="Error processing prompt"
+        )
+
+    # Get the response from the OpenAI API
+    openai_response = response.json()
+
+    # Return the OpenAI response to the original requester
+    return openai_response["choices"][0]["text"]
